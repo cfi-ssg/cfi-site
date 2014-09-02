c06152e6a744fc5cdd0c3d3cf249011007a41781
Kristen Bachman <kristen.bachman@udri.udayton.edu>
2014-09-01 21:33:48 -0400

    initial commit of ruby files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreativeFuse::Application.config.secret_key_base = '613d692d6d0c879f9de953bbdbad380969c0f5b8c43abcac1d83730eb7f6b4f39d42659e30be2c79af7b686de212b46d91874ff28f9b1ce29f938e56a9ef33f6'
